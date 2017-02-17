5f2125c007118e2b94fd314b08ac9480a349110f
waste143 <masihmalakooti12345@gmail.com>
2017-02-17 21:58:49 +0330

    bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -94,7 +94,7 @@ function create_config( )
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
-    info_text = [[》permag.ir v3
+    info_text = [[》VaRaQ TG v3
 
 🔰Our channel :
 》@MasihTgCh《
